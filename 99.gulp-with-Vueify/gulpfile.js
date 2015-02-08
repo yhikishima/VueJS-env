6270999d2e2a4681a3b736bf45980d16280f8bad
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-02-08 12:09:53 +0900

    add local server action task

--- 99.gulp-with-Vueify/gulpfile.js
+++ 99.gulp-with-Vueify/gulpfile.js
@@ -1,7 +1,17 @@
 var gulp  = require('gulp');
 var ect   = require('gulp-ect');
+var connect = require('gulp-connect');
 var runSequence = require('run-sequence');
 var browserify = require('gulp-browserify');
+
+// 簡易サーバー
+gulp.task('connect', function() {
+  return connect.server({
+    port: 3000,      // ポート番号を設定
+    root: 'dist/',   // ルートディレクトリの場所を指定
+    livereload: true // ライブリロードを有効にする
+  });
+});
 
 // ect
 gulp.task('ect', function(){
@@ -26,9 +36,21 @@ gulp.task('watch', function() {
 
 // browserifyのタスクのtransformにdebowerifyとvueifyを指定する
 gulp.task('browserify', function() {
-    gulp.src('src/js/main.js')
-        .pipe(browserify({transform: ["debowerify", "vueify"]}))
-        .pipe(gulp.dest('dist/js'));
+  gulp.src('src/js/main.js')
+    .pipe(browserify({transform: ["debowerify", "vueify"]}))
+    .pipe(gulp.dest('dist/js'));
+});
+
+// 開発用
+gulp.task('serve', [
+    'connect',
+    'ect'
+  ], function(){
+  // 依存関係があるため、runSequenceで順序通りタスクを実行
+  runSequence(
+    'browserify',
+    'watch'
+  );
 });
 
 // watchして変更があったらビルドを実行するタスクの定義
